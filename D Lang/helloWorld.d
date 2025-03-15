5a36f7b75755d91cc1702133082f10cb2d2ed636
Ayan Das <ayandazzz@outlook.com>
2025-03-15 12:38:51 +0530

    Added D programing Language Folder

---
+++ D Lang/helloWorld.d
@@ -0,0 +1,7 @@
+import std.stdio;
+
+void main() {
+    write("Hello, ");         // Prints without newline
+    writeln("D!");            // Continues on the same line
+    writeln("Hello, World!"); // Prints with newline
+}
---
+++ D Lang/looping.d
@@ -0,0 +1,9 @@
+import std.stdio;
+
+  void main()
+{
+    // int[] unused = new int[1000_000];
+    foreach(i; 0 .. 10) {
+      writeln("Hello");
+    }
+}
