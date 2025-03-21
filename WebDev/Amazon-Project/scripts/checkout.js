be166abdc68f6823c154d7e3d4391728c1ef9f8b
Ayan Das <ayandazzz@outlook.com>
2025-03-21 17:06:46 +0530

    Added Promises to render The Cart

--- WebDev/Amazon-Project/scripts/checkout.js
+++ WebDev/Amazon-Project/scripts/checkout.js
@@ -2,7 +2,12 @@ import { renderCheckoutCart } from './checkout/orders.js';
 import { renderPaymentSummary } from './checkout/payment.js';
 import { loadProducts } from '../data/products.js';
 
-loadProducts(renderPage);
+new Promise((resolve) => {
+  loadProducts(resolve);
+}).then(() => {
+  console.log('rendering Page');
+  renderPage();
+});
 
 export function renderPage() {
   renderCheckoutCart();
